732b2cbb719a4923271883d95ef6b27817bd79e2
Dawntwister <ngzhanhui9@gmail.com>
2024-11-06 15:24:52 +0800

    Add: CSV upload and retrieval

--- dashboardApp/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import jsonify
-
-def register_routes(app):
-    @app.route('/api/hello', methods=['GET'])
-    def hello_world():
-        return jsonify({"message": "Hello from Flask Docker!"})
